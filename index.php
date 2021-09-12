d67855bd5ba784d9f34523c6ed751b9c3c553d39
rebel76cz <79139996+rebel76cz@users.noreply.github.com>
2021-09-12 22:58:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<link rel="stylesheet" href="style.css">
+    <h1>Převody peněz</h1>
+    
+<br>
+
+    <h3>Částka</h3>
+
+<input id="number" type="number" value="">
+
+<br>
+<br>
+  
+<h3>Převod</h3>
+      <input type="radio" id="html" name="fav_language" value="Eur-Czk">
+       <label for="html">Eur-Czk</label><br>
+
+          <input type="radio" id="html" name="fav_language" value="Czk-Eur">
+           <label for="html">Czk-Eur</label><br>
+
+
+<h3>Převedená částka</h3>
+<input type="text">
+
+<br>
+
+
+
+
+
+</body>
+</html>
